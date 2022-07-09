61267156d15a476b302732eb878d1ba1610b6f6e
AlbertoLanchas <albertolsn7@gmail.com>
2022-07-09 10:12:40 +0200

    project 13 slide scroll upload
    Signed-off-by: AlbertoLanchas <albertolsn7@gmail.com>

--- 12-Key_sequence_detection/konami.js
+++ 12-Key_sequence_detection/konami.js
@@ -3,7 +3,7 @@ const secretCode = "musica";
 
 window.addEventListener("keyup", (e) => {
   pressed.push(e.key);
-  pressed.splice(-secretCode.lenght - 1, pressed.length - secretCode.length);
+  pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
   console.log(pressed);
   if (pressed.join("").includes("musica")) {
     cornify_add();
